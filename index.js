54d6afd9e12fa08d0316415d0f01f2d8fc979886
James Warwood <warwood.aj@gmail.com>
2020-08-29 08:09:31 +0100

    feat: Hack in a loadSync function

--- index.js
+++ index.js
@@ -10,183 +10,194 @@ var Input = require("./system/input");
 
 var config = require("./config.json");
 
-function System( el ) {
-	this.config = config;
+function System(el) {
+  this.config = config;
 
-	// system timing flags
-	this.frameEnded = false;
-	this.tickAPU = false;
+  // system timing flags
+  this.frameEnded = false;
+  this.tickAPU = false;
 
-	// IO
-	this.controllers = new Controllers();
-	this.input = new Input( this );
-	this.output = new Output();
+  // IO
+  this.controllers = new Controllers();
+  this.input = new Input(this);
+  this.output = new Output();
 
-	// video output
-	if ( el ) {
-		this.output.video.setElement( el );
-	}
+  // video output
+  if (el) {
+    this.output.video.setElement(el);
+  }
 
-	// reserve for timing
-	this.interval = null;
-	this.running = false;
-	this.paused = true;
+  // reserve for timing
+  this.interval = null;
+  this.running = false;
+  this.paused = true;
 
-	// reserve for system core
-	this.cartridge = null;
-	this.cpu = null;
-	this.apu = null;
-	this.ppu = null;
-	this.memory = null;
+  // reserve for system core
+  this.cartridge = null;
+  this.cpu = null;
+  this.apu = null;
+  this.ppu = null;
+  this.memory = null;
 
-	Object.preventExtensions( this );
+  Object.preventExtensions(this);
 }
 
 System.prototype = {
-	/**
-	 * Load a ROM and optionally run it.
-	 * @param {string} filename - Path of ROM to run.
-	 * @param autorun - If true, run ROM when loaded. If a function, call that function.
-	 */
-	load: function( filename, autorun ) {
-		var self = this;
+  /**
+   * Load a ROM and optionally run it.
+   * @param {string} filename - Path of ROM to run.
+   * @param autorun - If true, run ROM when loaded. If a function, call that function.
+   */
+  load: function(filename, autorun) {
+    var self = this;
 
-		utils.readFile( filename, function( data ) {
-			self.initCartridge( data );
+    utils.readFile(filename, function(data) {
+      self.initCartridge(data);
 
-			if ( typeof autorun === "function" ) {
-				autorun();
-			} else if ( autorun === true ) {
-				self.run();
-			}
-		});
-	},
+      if (typeof autorun === "function") {
+        autorun();
+      } else if (autorun === true) {
+        self.run();
+      }
+    });
+  },
 
-	/**
-	 * Turn on and run emulator.
-	 */
-	run: function() {
-		if ( this.interval ) {
-			// once is enough
-			return;
-		}
+  loadSync: function(filename, autorun) {
+    const data = utils.readFileSync(filename);
+    this.initCartridge(data);
 
-		var self = this;
-		this.interval = setInterval( function() {
-			if ( !self.paused) {
-				self.runFrame();
-			}
-		}, 1000 / 60 );
+    if (typeof autorun === "function") {
+      autorun();
+    } else if (autorun === true) {
+      this.run();
+    }
+  },
 
-		this.output.video.run();
+  /**
+   * Turn on and run emulator.
+   */
+  run: function() {
+    if (this.interval) {
+      // once is enough
+      return;
+    }
 
-		this.running = true;
-		this.paused = false;
-	},
+    var self = this;
+    this.interval = setInterval(function() {
+      if (!self.paused) {
+        self.runFrame();
+      }
+    }, 1000 / 60);
 
-	/**
-	 * Run a single frame (1/60s NTCS, 1/50s PAL).
-	 */
-	runFrame: function() {
-		var cpu = this.cpu,
-		    ppu = this.ppu,
-		    apu = this.apu;
+    this.output.video.run();
 
-		while ( !ppu.frameEnded ) {
-			cpu.tick();
+    this.running = true;
+    this.paused = false;
+  },
 
-			ppu.tick();
-			ppu.tick();
-			ppu.tick();
+  /**
+   * Run a single frame (1/60s NTCS, 1/50s PAL).
+   */
+  runFrame: function() {
+    var cpu = this.cpu,
+      ppu = this.ppu,
+      apu = this.apu;
 
-			if ( this.tickAPU ) {
-				apu.tick();
-			}
-			this.tickAPU = !this.tickAPU;
-		}
+    while (!ppu.frameEnded) {
+      cpu.tick();
 
-		ppu.frameEnded = false;
-	},
+      ppu.tick();
+      ppu.tick();
+      ppu.tick();
 
-	/**
-	 * Synchronously simulate running for a number of milliseconds.
-	 * @param {number} milliseconds - The number of milliseconds to simulate.
-	 */
-	simulate: function( milliseconds ) {
-		var i,
-		    frames = ( milliseconds / 1000 ) * 60;
+      if (this.tickAPU) {
+        apu.tick();
+      }
+      this.tickAPU = !this.tickAPU;
+    }
 
-		for ( i = 0; i < frames; i++ ) {
-			this.runFrame();
-		}
-	},
+    ppu.frameEnded = false;
+  },
 
-	/**
-	 * Resume running.
-	 */
-	play: function() {
-		this.paused = false;
+  /**
+   * Synchronously simulate running for a number of milliseconds.
+   * @param {number} milliseconds - The number of milliseconds to simulate.
+   */
+  simulate: function(milliseconds) {
+    var i,
+      frames = (milliseconds / 1000) * 60;
 
-		if ( !this.running ) {
-			this.run();
-		}
-	},
+    for (i = 0; i < frames; i++) {
+      this.runFrame();
+    }
+  },
 
-	/**
-	 * Stop running.
-	 */
-	pause: function() {
-		this.paused = true;
-	},
+  /**
+   * Resume running.
+   */
+  play: function() {
+    this.paused = false;
 
-	/**
-	 * On/off switch.
-	 */
-	toggle: function() {
-		if ( this.paused ) {
-			this.play();
-		} else {
-			this.pause();
-		}
+    if (!this.running) {
+      this.run();
+    }
+  },
 
-		return this.paused;
-	},
+  /**
+   * Stop running.
+   */
+  pause: function() {
+    this.paused = true;
+  },
 
-	/**
-	 * Initialize cartridge and hook into system.
-	 */
-	initCartridge: function( data ) {
-		this.cartridge = new Cartridge( data, this );
-	
-		this.initCore();
-		this.reset();
-	},
+  /**
+   * On/off switch.
+   */
+  toggle: function() {
+    if (this.paused) {
+      this.play();
+    } else {
+      this.pause();
+    }
 
-	loadCartridge: function( cartridge ) {
-		//this.cartridge = cartridge;
-		this.memory.loadCartridge( cartridge );
-		this.ppu.memory.loadCartridge( cartridge );
-	},
+    return this.paused;
+  },
 
-	/**
-	 * Initialize the core of our emulator (processor etc).
-	 */
-	initCore: function() {
-		this.cpu = new CPU( this );
-		this.apu = new APU( this );
-		this.ppu = new PPU( this );
-		this.memory = new Memory( this );
+  /**
+   * Initialize cartridge and hook into system.
+   */
+  initCartridge: function(data) {
+    this.cartridge = new Cartridge(data, this);
 
-		this.loadCartridge( this.cartridge );
-	},
+    this.initCore();
+    this.reset();
+  },
 
-	/**
-	 * Reset the console.
-	 */
-	reset: function() {
-		this.cpu.reset();
-		this.apu.reset();
-	}
+  loadCartridge: function(cartridge) {
+    //this.cartridge = cartridge;
+    this.memory.loadCartridge(cartridge);
+    this.ppu.memory.loadCartridge(cartridge);
+  },
+
+  /**
+   * Initialize the core of our emulator (processor etc).
+   */
+  initCore: function() {
+    this.cpu = new CPU(this);
+    this.apu = new APU(this);
+    this.ppu = new PPU(this);
+    this.memory = new Memory(this);
+
+    this.loadCartridge(this.cartridge);
+  },
+
+  /**
+   * Reset the console.
+   */
+  reset: function() {
+    this.cpu.reset();
+    this.apu.reset();
+  }
 };
 
 module.exports = System;
--- utils.js
+++ utils.js
@@ -4,13 +4,20 @@ var fs = require("fs");
  * Wrapper around fs.readFile to make it easily replaceble by Browserify and to
  * handle converting to an ArrayBuffer in a single place.
  */
-exports.readFile = function( filename, callback ) {
-	fs.readFile( filename, function( err, data ) {
-		if ( err ) {
-			throw err;
-		}
+exports.readFile = function(filename, callback) {
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      throw err;
+    }
 
-		var buffer = new Uint8Array( data ).buffer;
-		callback( buffer );
-	});
+    var buffer = new Uint8Array(data).buffer;
+    callback(buffer);
+  });
+};
+
+exports.readFileSync = function(filename) {
+  const data = fs.readFileSync(filename);
+  const { buffer } = new Uint8Array(data);
+
+  return buffer;
 };
